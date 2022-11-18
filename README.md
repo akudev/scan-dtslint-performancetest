7cf31199fb030ebcffb37bdc117e29a325e91fe1
Andreas Kunz <andreas.kunz@sap.com>
2022-11-18 12:08:09 +0100

    Initial import

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# scan-dtslint-performancetest
+# Dtslint Scan Performancetest
+
+## Usage
+
+```sh
+git clone https://github.com/akudev/scan-dtslint-performancetest.git
+cd scan-dtslint-performancetest
+npm install
+npm start
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+const { spawnSync } = require("child_process");
+const path = require("path");
+
+const start = Date.now();
+console.log(`Running... this can take afew minutes...`);
+
+// run dtslint
+spawnSync("npx", ["dtslint", ".", "2>", path.relative(process.cwd(), "output.txt")], { shell: true, maxBuffer: 1024 * 1024 * 20 }); // default max buffer size is 1 MB since 2019, was 200 kB at some earlier time; we need MORE!
+
+const end = Date.now();
+console.log(`Completed in ${((end - start) / 1000).toFixed(1)} seconds.`);
+

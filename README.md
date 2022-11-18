c769b9ea64d95bd072670442282ba82ae12069aa
Andreas Kunz <andreas.kunz@sap.com>
2022-11-18 12:13:08 +0100

    Improve details

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## Usage
 
+Prerequisites:
+- git client
+- Current NodeJS (>=16.x) with npm
+
 ```sh
 git clone https://github.com/akudev/scan-dtslint-performancetest.git
 cd scan-dtslint-performancetest
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { spawnSync } = require("child_process");
 const path = require("path");
 
 const start = Date.now();
-console.log(`Running... this can take afew minutes...`);
+console.log(`Running... this can take a few minutes...`);
 
 // run dtslint
 spawnSync("npx", ["dtslint", ".", "2>", path.relative(process.cwd(), "output.txt")], { shell: true, maxBuffer: 1024 * 1024 * 20 }); // default max buffer size is 1 MB since 2019, was 200 kB at some earlier time; we need MORE!
